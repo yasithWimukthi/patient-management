758b0f0cf661cf083a8deda5399ffa569c69675a
yasithwimukthi <wimukthiyasith@gmail.com>
2025-03-28 22:55:59 +0530

    create kafka producer

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,13 @@
+package com.pm.patientservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+}
